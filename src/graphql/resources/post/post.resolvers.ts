190f47b8ade53830a4865db1c479b3d95baad3e5
gadielkalleb <gadiel_kalleb@hotmail.com>
2019-01-20 17:39:30 -0200

    criação de uma função auxilia

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -2,8 +2,23 @@ import { GraphQLResolveInfo } from "graphql";
 
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { PostInstances } from "../../../models/PostModel";
+import { Transaction } from "sequelize";
+import { error } from "util";
 
-export const postResolvers = {
+const findByIdTransaction = (id, db) => {
+  return new Promise((resolve, reject) => (
+    db.sequelize.transaction((t: Transaction) => (
+      db.Post.findById(parseInt(id)).then((post: PostInstances) => {
+        if (!post) {
+          reject({ error: new Error(`Post with id ${id} not found!`)} )
+        }
+        resolve({ post, t })
+      })
+    ))
+  ))
+}
+
+export const postResolvers = { 
   Post: {
     author: (post, args, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
       return db.User.findById(post.get('author'));
@@ -30,6 +45,41 @@ export const postResolvers = {
         if (!post) throw new Error(`Post with id ${id} not found!`);
         return post;
       });
+    },
+  },
+
+  Mutation: {
+    createPost: (parent, { input }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.Post.create(input, { transaction: t });
+      })
+    },
+    updatePost: (parent, { id, input }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => (
+      findByIdTransaction(id, db)
+        .then(({post, t}) => post.update(input, { transaction: t }))
+        .catch(({ error }) => {
+          return error
+        })
+      // db.sequelize.transaction((t: Transaction) => (
+      //   db.Post.findById(parseInt(id)).then((post: PostInstances) => {
+      //     if (!post) throw new Error(`Post with id ${id} not found!`);
+      //     return post.update(input, { transaction: t })
+      //   })
+      // ))
+    ),
+    deletePost: (parent, { id }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+      findByIdTransaction(id, db)
+        .then(({post, t}) => post.destroy({ transaction: t }).then(post => !!post))
+        .catch(({ error }) => {
+          return error
+        })
+      
+      // return db.sequelize.transaction((t: Transaction) => {
+      //   return db.Post.findById(parseInt(id)).then((post: PostInstances) => {
+      //     if (!post) throw new Error(`Post with id ${id} not found!`);
+      //     return post.destroy({ transaction: t }).then(post => !!post)
+      //   })
+      // })
     },
   }
 }
