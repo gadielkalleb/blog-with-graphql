5367de9fd115bf6fcf0e5d64b60491e8eac17a28
gadielkalleb <gadiel_kalleb@hotmail.com>
2019-03-30 17:58:07 -0300

    refact para async/await

--- src/graphql/resources/token/token.resolvers.ts
+++ src/graphql/resources/token/token.resolvers.ts
@@ -7,22 +7,24 @@ import { JWT_SECRET } from '../../../utils/utils';
 export const tokenResolvers = {
 
   Mutation: {
-    createToken: (parent, { email, password }, {db}: {db: DbConnection}) => {
-      return db.User.findOne({
-        where: { email: email },
-        attributes: ['id', 'password']
-      })
-      .then((user: UserInstance) => {
+    createToken:
+      async (parent, { email, password }, { db }: { db: DbConnection }) => {
         const errorMessage = 'Unautorized, wrong email or password';
-        if (!user || !user.isPassword(user.get('password'), password)) { 
+        const user: UserInstance = await db.User.findOne({
+          where: { email: email },
+          attributes: ['id', 'password']
+        })
+
+        if (!user || !user.isPassword(user.get('password'), password)) {
           throw new Error(errorMessage);
         }
-        const payload = { sub: user.get('id')};
+        const payload = { sub: user.get('id') };
 
         return {
-          token: jwt.sign(payload, JWT_SECRET, { expiresIn: '1d'})
+          token: jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' })
         }
-      })
-    }
+
+      }
   }
+  
 }
